110593e379c90bb4863928bfdec702f7bda151e7
THIS IS MY NAME <github@0x3d.uk>
2022-12-03 11:37:29 +0000

    Cleaned up code
    Note that I have made a few changes to the code to improve its readability and compatibility with Python 3. In particular, I have added a shebang at the top of the script to specify the Python interpreter, changed the print statements to use the correct syntax in Python 3, and used f-strings to interpolate variables in the string values passed to sendline and expect.

--- universal.py
+++ universal.py
@@ -1,23 +1,20 @@
-#!/usr/bin/python3
-import os
-import subprocess
+#!/usr/bin/env python3
 import platform
 import pexpect
 import time
-import sys
 
 os = platform.system()
 
 if os == "Windows":
-	print("Windows is detected")
+    print("Windows is detected")
 
 elif os == "Linux":
-	print("Linux is detected")
+    print("Linux is detected")
 
 elif os == "Darwin":
-	Print("MacOS detected")
+    print("MacOS detected")
 else:
-	Print("Unsure what os is running")
+    print("Unsure what os is running")
 
 
 print("Ensure you have the mac address of the device you wish to attack")
@@ -27,21 +24,21 @@ dildo = input("")
 
 
 if dildo == ("flam"):
-	dildo = ("50:F1:4A:56:70:11")
-	print("Flamingo mode")
+    dildo = ("50:F1:4A:56:70:11")
+    print("Flamingo mode")
 
 if dildo == ("verge"):
-	dildo = ("50:8C:B1:44:8A:AB")
-	print("Verge mode")
+    dildo = ("50:8C:B1:44:8A:AB")
+    print("Verge mode")
 
 print("Enter the vibration")
 vibration = input("")
 
 print("gatttool is starting up")
 child = pexpect.spawn("gatttool -I")
-child.sendline("connect {0}".format(dildo))
+child.sendline(f"connect {dildo}")
 child.expect("Connection successful"),
-command="char-write-req {0}".format(vibration)
+command=f"char-write-req {vibration}"
 time.sleep(5)
 child.sendline(command)
 child.expect("Characteristic value was written successfully", timeout=10)
